b3f74a72e47b638ef8edff0b8abd0556fcf25e33
lch0203 <59508381@qq.com>
2018-05-11 15:10:00 +0800

    Initial commit

---
+++ src/main/java/cn/ynou/casmanagement/CasmanagementApplication.java
@@ -0,0 +1,12 @@
+package cn.ynou.casmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasmanagementApplication.class, args);
+    }
+}
---
+++ src/test/java/cn/ynou/casmanagement/CasmanagementApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.ynou.casmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CasmanagementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
